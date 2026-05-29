62c326bb726b7d3cbf5b14c1c3533fda92459f3e
Vedant Tapiavala <vtapiavala2005@gmail.com>
2026-05-29 02:41:28 -0400

    Move prune_redundant_features to utils

--- code/utils.py
+++ code/utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import roc_auc_score, average_precision_score
+import networkx as nx
 
 def convert_csv_parquet(filename, dtype_arr, usecols) -> None:
     df = pd.read_csv(f'{filename}.csv', dtype=dtype_arr,  usecols=usecols)
@@ -16,10 +17,65 @@ def convert_csv_parquet(filename, dtype_arr, usecols) -> None:
     assert df.columns == df_parquet.columns, 'Different Number of Columns in Resulting Parquet'
     assert len(df) == len(df_parquet), 'Different Number of Rows in Resulting Parquet'
 
+def prune_redundant_features(X):
+    corr = X.corr(method='pearson')
+
+    upper = corr.where(
+        np.triu(np.ones(corr.shape), k=1).astype(bool)
+    )
+
+    corr_df = (
+        upper.stack()
+        .reset_index()
+        .rename(columns={
+            'level_0': 'Feature 1',
+            'level_1': 'Feature 2',
+            0: 'Pearson Correlation'
+        })
+    )
+
+    corr_df['Abs Correlation'] = corr_df['Pearson Correlation'].abs()
+    corr_df = corr_df.sort_values(by='Abs Correlation', ascending=False)
+
+    results_adj = corr_df[corr_df['Abs Correlation'] >= 0.8]
+    edges = [(f1, f2) for f1, f2 in zip(results_adj['Feature 1'], results_adj['Feature 2'])]
+    G = nx.Graph()
+    G.add_edges_from(edges)
+
+    covered = set()
+    features = set(G.nodes())
+
+    centrality = nx.degree_centrality(G)
+
+    uncovered_neighbors = {f: len(list(G.neighbors(f))) for f in features}
+
+    kept_features = []
+
+    while len(covered) < len(features):
+        candidates = [f for f in features if f not in kept_features]
+        max_neighbors = max(uncovered_neighbors[f] for f in candidates)
+        top_candidates = [f for f in candidates if uncovered_neighbors[f] == max_neighbors]
+
+        # Tie break based on degree certainty if necessary
+        if len(top_candidates) > 1:
+            top_candidates.sort(key = lambda x: centrality[x], reverse=True)
+
+        chosen = top_candidates[0]
+
+        kept_features.append(chosen)
+
+        covered.update([chosen] + list(G.neighbors(chosen)))
+
+    for f in candidates:
+        if f not in kept_features:
+            uncovered_neighbors[f] = len([n for n in G.neighbors(f) if n not in covered])
+    
+    return kept_features, set(features) - set(kept_features)
+
 def test_model(df_model):
     assert 'gameDateTimeEst_player' in df_model.columns, 'Game Date/Time Not Found'
     assert 'injury_within_14d' in df_model.columns, 'Target Variable for Injury Not Found'
-    
+
     train = df_model[df_model['gameDateTimeEst_player'] < '2023-10-01'].copy()
     test  = df_model[df_model['gameDateTimeEst_player'] >= '2023-10-01'].copy()
 
