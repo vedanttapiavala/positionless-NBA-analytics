97c7e51b0c1b3e24a80d0b1001e002b8919e612d
Vedant Tapiavala <vtapiavala2005@gmail.com>
2026-05-29 01:54:38 -0400

    Updating Injury-Related Features

--- code/011_injury_prediction_log_reg.py
+++ code/011_injury_prediction_log_reg.py
@@ -6,8 +6,7 @@ injury_features = ['home_player', 'positionless_index',
                    'rolling_7g_minutes', 'rolling_7g_points_per36',
                    'rolling_7g_three_pointers_attempted',
                    'rolling_7g_assists_per36',
-                   'rolling_7g_field_goals_attempted',
-                   'back_to_back',
+                   'rolling_7g_USG', 'rolling_7g_three_pointers_made',
                    'games_last_14d', 'distance_miles',
                    'tz_shift_hrs', 'rest_days']
 df_injury_features = df[injury_features + ['gameDateTimeEst_player', 'injury_within_14d']]
@@ -15,7 +14,7 @@ or_df, auc, ap = test_model(df_injury_features.dropna())
 
 print(or_df)
 
-# ROC-AUC: 0.5993516
-# PR-AUC: 0.257589
+# ROC-AUC: 0.5883995306289127
+# PR-AUC: 0.24835901752561373
 # Positionless index is still significant
 print(auc, ap)
